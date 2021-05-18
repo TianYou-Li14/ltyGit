712f2c96f9a14f9f63381c56766d6bbea5e0fc74
TianYou-Li14 <1137458293@qq.com>
2021-05-18 15:43:02 +0800

    初次修改该项目

--- src/com/git/TestGitDemo1.java
+++ src/com/git/TestGitDemo1.java
@@ -3,5 +3,6 @@ package com.git;
 public class TestGitDemo1 {
     public static void main(String[] args) {
         System.out.println("这是TestGitDemo1");
+        System.out.println("进行了第一次修改");
     }
 }
